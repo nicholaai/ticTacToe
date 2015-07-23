cf24ebececeee622c7bbb38099e99cadc0203dfc
Nicholas Pappagalo <npappagalo@gmail.com>
2015-07-23 00:28:31 -0700

    Added reset button to clear entries

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -2,22 +2,27 @@ $(function() {
   var $squares = $('.square');
   var turn = 0;
 
-  function isEmpty(el) {              // Check if element is empty
-    return !$.trim(el.html());         // Ignore invisible elements
+  function isEmpty(el) {                // Check if element is empty
+    return !$.trim(el.html());          // Ignore invisible elements
   }
 
-  $squares.on('click', function() {   // Add event handler
-    var $this = $(this);              // Store item clicked
+  $squares.on('click', function() {     // Add event handler
+    var $this = $(this);                // Store item clicked
 
-    if (isEmpty($this)) {             // If the square is empty
-      if (turn % 2 === 0) {           // If remainder of turn divided by 2 is 0
-        $this.html('X');              // Mark with X
+    if (isEmpty($this)) {               // If the square is empty
+      if (turn % 2 === 0) {             // If remainder of turn divided by 2 is 0
+        $this.html('X');                // Mark with X
       } else {
-        $this.html('O');              // Mark with O
+        $this.html('O');                // Mark with O
       }
-      turn += 1;                      // Add one to turn
+      turn += 1;                        // Add one to turn
     }
+  });
 
+  $('#reset').on('click', function() {  // When user clicks reset button
+    $squares.each(function() {          // Loop through squares
+      this.innerHTML = '';              // Clear content
+    });
   });
 
 });
